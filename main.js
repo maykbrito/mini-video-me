d1e96eea4865ceaa95d9be0f19dd718578568573
maykbrito <maykbrito@gmail.com>
2020-09-05 20:44:15 -0300

    :tada: initial commit

---
+++ main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow } = require('electron')
+
+const width = 250
+const height = 250
+function createWindow () {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width,
+    height,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    frame: false,
+    titleBarStyle: "customButtonsOnHover",
+    transparent: true,
+    alwaysOnTop: true,
+    resizable: false,
+    movable:true,
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+  // win.setIgnoreMouseEvents(true) // mouse will not working
+  win.setPosition( 1920 - width, 1080 - height, true)
+
+
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(createWindow)
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
