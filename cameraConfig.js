443a6e697d51e75b37dfeb21bb5cc1209ee960c9
Diego Fernandes <diego.schell.f@gmail.com>
2021-05-02 20:42:57 -0300

    chore: Create tray menu

--- cameraConfig.js
+++ cameraConfig.js
@@ -1,13 +1,16 @@
-export default {
-    width: 1920,
-    height: 1080,
-    aspectRatio: 1.77778,
-    frameRate: 59.94,
-    flipHorizontal: false, // start flipped
-    rounded: true, // border radius for camera
-    scale: 1.1, // zoom?
-    // move point of interest
-    // number will be converted to percent
-    horizontal: "-20", // 0 left - translateX
-    vertical: "0", // 0 top - translateY
+const { userPreferences } = require('./src/store')
+
+const store = userPreferences.store;
+
+const config = {
+  width: store.camera.width,
+  height: store.camera.height,
+  frameRate: store.camera.frameRate,
+  flipHorizontal: store.flipHorizontal,
+  rounded: store.rounded,
+  scale: store.zoom,
+  horizontal: String(store.anchor.x),
+  vertical: String(store.anchor.y),
 }
+
+export { config };
