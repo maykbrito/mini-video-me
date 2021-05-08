3a417dbad1f66927a6b5e10f448d3e1ac024b192
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-05-08 11:34:31 -0300

    add my camera device

--- cameraConfig.js
+++ cameraConfig.js
@@ -3,6 +3,7 @@ export default {
     height: 1080,
     aspectRatio: 1.77778,
     frameRate: 59.94,
+    deviceId: "4d61ab3b4054d24b7db8acc628427951de7344a32d308fb04d6bef5dc80ba59f",
     flipHorizontal: false, // start flipped
     rounded: true, // border radius for camera
     scale: 1.1, // zoom?
