9d997a475359a379497a4c53c7c5b5eedf5e8381
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-05-08 12:31:18 -0300

    format style

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-import cameraConfig from './cameraConfig.js';
-import Cam from './cam.js';
+import cameraConfig from './cameraConfig.js'
+import Cam from './cam.js'
 
 navigator.mediaDevices
-  .enumerateDevices()
-  .then((devices) => {
-    const videoDevices = devices.filter((device) => {
-      return device.kind === 'videoinput';
+    .enumerateDevices()
+    .then((devices) => {
+        const videoDevices = devices.filter((device) => {
+        return device.kind === 'videoinput'
     });
 
     // Using webcam in browser
     navigator.mediaDevices
-      .getUserMedia({
-        video: { ...cameraConfig, deviceId: videoDevices[0].deviceId } || true
-      })
-      .then((stream) => (Cam.video.srcObject = stream));
+        .getUserMedia({
+            video: { ...cameraConfig, deviceId: videoDevices[0].deviceId } || true
+        })
+        .then((stream) => (Cam.video.srcObject = stream))
 
     // Start Controlling Cam
     Cam.init();
 
-    let counter = 1;
+    let counter = 1
     // Keyboard Events
     window.addEventListener('keydown', (e) => {
-      if (Cam.controls[e.key]) {
-        Cam.controls[e.key]();
-      }
+        if (Cam.controls[e.key]) {
+            Cam.controls[e.key]()
+        }
 
-      if (e.key === 'c') {
-        navigator.mediaDevices
-          .getUserMedia({
-            video:
-              { ...cameraConfig, deviceId: videoDevices[counter].deviceId } ||
-              true
-          })
-          .then((stream) => (Cam.video.srcObject = stream));
+        if (e.key === 'c') {
+            navigator.mediaDevices
+            .getUserMedia({
+                video:
+                { ...cameraConfig, deviceId: videoDevices[counter].deviceId } ||
+                true
+            })
+            .then((stream) => (Cam.video.srcObject = stream));
 
-        counter++;
+        counter++
 
         if (counter >= videoDevices.length) {
-          counter = 0;
+            counter = 0
+            }
         }
-      }
-    });
-  });
+    })
+    })
