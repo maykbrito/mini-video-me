b29bfbe0d14be0aa2a47fe12d5f79dea4be0c338
Luiz Fernando Veríssimo <62751844+luizfverissimo@users.noreply.github.com>
2021-05-22 10:34:30 -0300

    Add video input source selector on tray menu.

--- index.js
+++ index.js
@@ -1,48 +1,40 @@
-import cameraConfig from './cameraConfig.js'
-import Cam from './cam.js'
+const { ipcRenderer } = require('electron');
 
-navigator.mediaDevices
-    .enumerateDevices()
-    .then((devices) => {
-        const videoDevices = devices.filter((device) => {
-        return device.kind === 'videoinput'
-    });
+import cameraConfig from './cameraConfig.js';
+import Cam from './cam.js';
 
-    // Using webcam in browser
+navigator.mediaDevices.enumerateDevices().then((devices) => {
+  const videoDevices = devices.filter((device) => {
+    return device.kind === 'videoinput';
+  });
+
+  // Using webcam in browser
+  navigator.mediaDevices
+    .getUserMedia({
+      video: { ...cameraConfig, deviceId: videoDevices[0].deviceId } || true
+    })
+    .then((stream) => (Cam.video.srcObject = stream));
+
+  // Start Controlling Cam
+  Cam.init();
+
+  ipcRenderer.send('videoInput', JSON.stringify(videoDevices));
+  ipcRenderer.on('videoInputChange', (event, videoInputIndex) => {
     navigator.mediaDevices
-        .getUserMedia({
-            video: { ...cameraConfig, deviceId: videoDevices[0].deviceId } || true
-        })
-        .then((stream) => (Cam.video.srcObject = stream))
+      .getUserMedia({
+        video:
+          {
+            ...cameraConfig,
+            deviceId: videoDevices[videoInputIndex].deviceId
+          } || true
+      })
+      .then((stream) => (Cam.video.srcObject = stream));
+  });
 
-    // Start Controlling Cam
-    Cam.init();
-
-    let counter = 1
-    // Keyboard Events
-    window.addEventListener('keydown', (e) => {
-        if (Cam.controls[e.key]) {
-            Cam.controls[e.key]()
-        }
-
-        if (e.key === 'c') {
-            if(videoDevices.length === 0) {
-                return
-            }
-
-            navigator.mediaDevices
-            .getUserMedia({
-                video:
-                { ...cameraConfig, deviceId: videoDevices[counter].deviceId } ||
-                true
-            })
-            .then((stream) => (Cam.video.srcObject = stream));
-
-        counter++
-
-        if (counter >= videoDevices.length) {
-            counter = 0
-            }
-        }
-    })
-    })
+  // Keyboard Events
+  window.addEventListener('keydown', (e) => {
+    if (Cam.controls[e.key]) {
+      Cam.controls[e.key]();
+    }
+  });
+});
--- main.js
+++ main.js
@@ -1,66 +1,123 @@
-const { app, BrowserWindow, ipcMain} = require('electron')
-const customSize = 300
-const bigFactor = 2.4 // how much enlarge when double click video
-let win, smallPosition, bigPosition
+const { app, BrowserWindow, Tray, Menu, ipcMain } = require('electron');
+const path = require('path');
+const customSize = 300;
+const bigFactor = 2.4; // how much enlarge when double click video
+let win, smallPosition, bigPosition, tray, videoInputArrayFormatted;
 
 const updatePositions = {
-  'big': () => {
+  big: () => {
     // memo small position
-    smallPosition = win.getBounds()
+    smallPosition = win.getBounds();
 
-    if(!bigPosition) {
+    if (!bigPosition) {
       // first time enter, configure big video
-      const { x, y } = smallPosition
-      const proportion = customSize * bigFactor
-      bigPosition = {x: x - proportion, y: y - proportion, width: proportion, height: proportion}
+      const { x, y } = smallPosition;
+      const proportion = customSize * bigFactor;
+      bigPosition = {
+        x: x - proportion,
+        y: y - proportion,
+        width: proportion,
+        height: proportion
+      };
     }
 
     // move and make it bigger
-    win.setBounds(bigPosition, true)
+    win.setBounds(bigPosition, true);
   },
-  'small': () => {
+  small: () => {
     // memo big position
-    bigPosition = win.getBounds()
-    
+    bigPosition = win.getBounds();
+
     // move and make it smaller
-    win.setBounds(smallPosition, true)
+    win.setBounds(smallPosition, true);
   }
-}
+};
 
 ipcMain.on('double-click', (event, arg) => {
-  const size = arg ? 'small' : 'big'
-  updatePositions[size]()
-})
+  const size = arg ? 'small' : 'big';
+  updatePositions[size]();
+});
 
-function createWindow () {
+function createWindow() {
   win = new BrowserWindow({
     width: customSize,
     height: customSize,
     frame: false,
-    titleBarStyle: "customButtonsOnHover",
+    titleBarStyle: 'customButtonsOnHover',
     transparent: true,
     alwaysOnTop: true,
     maximizable: false,
     webPreferences: {
       nodeIntegration: true
     }
-  })
+  });
 
   win.loadFile('index.html');
-  win.setVisibleOnAllWorkspaces(true)
-
+  win.setSkipTaskbar(true); //remove application from task bar
+  win.setVisibleOnAllWorkspaces(true);
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
+
+function createTray(videoInputMenuItens) {
+  tray = new Tray(path.join(__dirname, 'assets', 'webcam.ico'));
+
+  tray.setToolTip('Mini Video Me');
+  tray.setContextMenu(trayMenuBuilder(videoInputMenuItens));
+}
+
+function trayMenuBuilder(videoInputArray) {
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Video Input Source', submenu: videoInputArray },
+    {
+      label: 'Quit',
+      click() {
+        app.quit();
+      }
+    }
+  ]);
+  return contextMenu;
+}
+
+function menuItemBuilder(label, index) {
+  return {
+    label: label,
+    type: 'radio',
+    checked: index === 0,
+    click() {
+      win.webContents.send('videoInputChange', index);
+      updateMenuTray(index);
+    }
+  };
+}
+
+function updateMenuTray(clickedIndex) {
+  videoInputArrayFormatted.forEach((item, index) => {
+    return {
+      ...item,
+      checked: index === clickedIndex
+    };
+  });
+}
+
+ipcMain.on('videoInput', (event, arg) => {
+  const videoInputArray = JSON.parse(arg);
+
+  videoInputArrayFormatted = videoInputArray.map((input, index) => {
+    return menuItemBuilder(input.label, index);
+  });
+
+  createTray(videoInputArrayFormatted);
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
