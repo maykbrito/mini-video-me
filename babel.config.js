7d1d702a2c79ab7b34eb97a2ce5d3785a9a3ebd2
Diego Fernandes <diego.schell.f@gmail.com>
2021-05-28 15:35:21 -0300

    chore: Add lint workflow

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,4 @@
 module.exports = {
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-typescript'
-  ],
-  plugins: [
-    '@babel/transform-runtime'
-  ]
+  presets: ['@babel/preset-env', '@babel/preset-typescript'],
+  plugins: ['@babel/transform-runtime'],
 }
