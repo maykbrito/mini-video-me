1e954962dfc5f1ef75d177526d286bc4837c82db
Mayk Brito <maykbrito@gmail.com>
2021-11-15 09:22:20 -0300

    chore: add filters selection logic

--- src/cam.ts
+++ src/cam.ts
@@ -8,18 +8,24 @@ export class CameraController {
   public videoElement: HTMLVideoElement
 
   private wrapperElement: HTMLDivElement
+  private wrapperVideoElement: HTMLDivElement
   private isFlipped: boolean
   private currentShapePosition: number
+  private currentFilterPosition: number
   private position: Record<'x' | 'y' | 'z', number>
   private root: HTMLElement
 
   constructor() {
     this.wrapperElement = document.getElementById('wrapper') as HTMLDivElement
+    this.wrapperVideoElement = document.getElementById(
+      'video-wrapper'
+    ) as HTMLDivElement
     this.videoElement = document.getElementById('video') as HTMLVideoElement
     this.root = document.querySelector(':root') as HTMLElement
 
     this.isFlipped = config.flipHorizontal
     this.currentShapePosition = -1
+    this.currentFilterPosition = -1
 
     this.position = {
       x: config.horizontal,
@@ -73,6 +79,10 @@ export class CameraController {
     this.applyShape()
   }
 
+  public toggleFilters() {
+    this.applyFilter()
+  }
+
   private applyPositioning() {
     this.videoElement.style.transform = `translate(${this.position.x}%, ${this.position.y}%) scale(${this.position.z})`
 
@@ -108,10 +118,33 @@ export class CameraController {
     return config.shapes[this.currentShapePosition]
   }
 
+  private applyFilter() {
+    const filter = this.getFilter()
+
+    if (filter.includes('blend-')) {
+      this.root.style.setProperty('--filter', 'initial')
+      this.wrapperVideoElement.setAttribute('class', filter)
+    } else {
+      this.wrapperVideoElement.setAttribute('class', '')
+      this.root.style.setProperty('--filter', filter)
+    }
+  }
+
+  private getFilter() {
+    this.currentFilterPosition++
+
+    if (config.filters.length <= this.currentFilterPosition) {
+      this.currentFilterPosition = 0
+    }
+
+    return config.filters[this.currentFilterPosition]
+  }
+
   private render() {
     this.applyPositioning()
     this.flipHorizontal()
     this.applyBorder()
     this.applyShape()
+    this.applyFilter()
   }
 }
