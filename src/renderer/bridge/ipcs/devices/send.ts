d7b3fc30bed6a691d57d420ced0272a3d4ad1650
Davi Rodrigues <developer.davi@gmail.com>
2023-02-09 17:16:06 -0300

    feat: add permanent storage for deviceId

--- src/renderer/bridge/ipcs/devices/send.ts
+++ src/renderer/bridge/ipcs/devices/send.ts
@@ -12,5 +12,9 @@ export function sendVideoInputDeviceList(devices: VideoDevice[]) {
 export function sendActiveVideoInputId(id: string) {
   const { GET_ACTIVE_VIDEO_INPUT_ID } = IPC.DEVICES
 
+  if (id) {
+    localStorage.setItem('deviceId', id)
+  }
+
   ipcRenderer.send(GET_ACTIVE_VIDEO_INPUT_ID, id)
 }
